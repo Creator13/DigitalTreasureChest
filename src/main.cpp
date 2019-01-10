fe9bd423360100aaa17d757cae979cf291f969d9
Casper van Battum <cvanbattum@gmail.com>
2019-01-10 21:29:36 +0100

    Add pressure sensor reading

--- src/main.cpp
+++ src/main.cpp
@@ -1,18 +1,59 @@
 #include <Arduino.h>
-#include "DisplayDriver.h"
+#include "HDSPA22C_Driver.h"
 
-DisplayDriver* driver;
+HDSPA22C_Driver* driver;
+
+const int numReadings = 100;
+int readings[numReadings];
+int readIndex = 0;
+int total = 0;
+int average = 0;
+uint32_t lastReading = millis();
+uint32_t readTimeout = 10;
+
+void readPressure() {
+    int pressure = map(analogRead(A0), 275, 1023, 99, 0);
+
+    if (pressure > 99) {
+        pressure = 99;
+    }
+    else if (pressure < 0) {
+        pressure = 0;
+    }
+
+    // subtract the last reading:
+    total = total - readings[readIndex];
+    // read from the sensor:
+    readings[readIndex] = pressure;
+    // add the reading to the total:
+    total = total + readings[readIndex];
+    // advance to the next position in the array:
+    readIndex = readIndex + 1;
+
+    // if we're at the end of the array...
+    if (readIndex >= numReadings) {
+        // ...wrap around to the beginning:
+        readIndex = 0;
+    }
+
+    // calculate the average:
+    average = total / numReadings;
+}
 
 void setup() {
     Serial.begin(9600);
 
-    driver = new DisplayDriver();
-    driver->setCharacter(0, 'I');
-    driver->setCharacter(1, 'K');
+    driver = new HDSPA22C_Driver();
 }
 
 void loop() {
     driver->send();
-    // TODO buffer input and smooth/average
-    map(analogRead(A0), 255, 1023, 99, 0);
+
+    if (millis() > lastReading + readTimeout) {
+        readPressure();
+        lastReading = millis();
+    }
+
+    driver->setCharacter(0, average / 10 + '0');
+    driver->setCharacter(1, average % 10 + '0');
 }
